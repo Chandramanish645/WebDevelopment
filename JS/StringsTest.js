5b1300bd1c1bc3723060b0947d2c59e857fe593b
Manish Chandra (manicha2) <manicha2@cisco.com>
2016-08-20 20:49:58 +0530

    First Commit

---
+++ JS/StringsTest.js
@@ -0,0 +1,22 @@
+//var str = "Is the earth on earth heaven";
+
+function LetterCapitalize(str) { 
+var str = '=' + str + '=';
+ for(var i =0 ; i < str.length ; i++){
+     if(str[i].match(/[a-z]/i)!==null){
+         if(str[i+1]==='+' && str[i-1]==='+'){
+             return true;
+         }
+     }
+ }
+  return false; 
+         
+         
+}
+   
+// keep this function call here 
+LetterCapitalize("f++d+");
+
+
+
+
